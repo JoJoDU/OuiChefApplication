0fe4397dcbc73cdd886a79a75d60739c59d1fbbf
JoJoDU <qiaoqiandu@gmail.com>
2019-05-05 23:01:58 +0200

    using dynamoose to read and create table are very easy.

--- app_server/routes/pageCommande.js
+++ app_server/routes/pageCommande.js
@@ -2,22 +2,12 @@ var express = require('express');
 var router = express.Router();
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('config.json');
-//var data = require('../models/pageCommandeData');
+var Product = require('../models/pageCommandeData');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //console.log(data);  
-var db = new AWS.DynamoDB();
 
-var product = 
-{
-  Key:{
-    "viande":{S:"porc"},
-},
-  TableName:"Viande"
-};
-
-db.getItem(product,function(err,data){
+Product.get({title:"porc"},function(err,data){
     if (err) {
         console.log("Error", err);
       } else {
