c7d5b3b062e8d6a8f980d901e7ebb96187d0aebb
julienazj <julienazj@163.com>
2019-05-16 00:51:46 +0200

    node-fetch added

--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pageAceuil', { title: 'Recette dans Oui Chef' });
+  res.render('users', { title: 'Recette dans Oui Chef' });
 });
 
 module.exports = router;
