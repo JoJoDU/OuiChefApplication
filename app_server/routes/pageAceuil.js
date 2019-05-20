aab559ec2a99a126b9f87e7b81b4c9d51f62b4bb
julienazj <julienazj@163.com>
2019-05-20 23:41:06 +0200

    new change

--- app_server/routes/pageAceuil.js
+++ app_server/routes/pageAceuil.js
@@ -1,9 +1,35 @@
+
+
 var express = require('express');
 var router = express.Router();
-
+var decode = require('./decode.js');
+var exec = require('child_process').exec
+var host = 'https://ouichef.auth.us-east-2.amazoncognito.com/oauth2/token';
+var uri = 'http%3A%2F%2Flocalhost%3A3000%2F';
+var client_id = '16g26706ovmhfj4d3o08qnnso';
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  var token = "";
   res.render('pageAceuil', { title: 'Oui Chef' });
+  var uuid = req.query.code;
+  console.log('code = ' + uuid);
+  var cmd = "curl -X POST " + host + " -H 'Content-Type: application/x-www-form-urlencoded'  -d 'grant_type=authorization_code&redirect_uri=" + uri + "&code=" + uuid + "&client_id=" + client_id + "'"
+  exec(cmd, function (err, stdout, stderr) {
+    
+      if (err) {
+        console.log(stderr);
+        //return stderr;
+      }
+      else {
+        console.log('return data: ' + JSON.parse(stdout));
+        token = JSON.parse(stdout).id_token;
+        console.log('token: '+token);
+        decode(token);
+        //console.log('noew token: '+jose.util.base64url.decode(token));
+        //console.log(JSON.parse(jose.util.base64url.decode(token)))
+      }
+    })
 });
+
 
 module.exports = router;
--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -3,12 +3,28 @@ var router = express.Router();
 global.fetch = require('node-fetch');
 var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 
+/*
+var poolData = {
+  UserPoolId : 'us-east-2_tjXnjbURk', // your user pool id here
+  ClientId : '4tkakr53s85hrt7mpqve70bi0v' // your app client id here
+};
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+var userData = {
+  Username : 'test', // your username here
+  Pool : userPool
+};
+*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('users', { title: 'Recette dans Oui Chef' });
 });
+router.post('/', function(req, res, next) {
+  res.redirect('https://ouichef.auth.us-east-2.amazoncognito.com/login?response_type=code&client_id=16g26706ovmhfj4d3o08qnnso&redirect_uri=http://localhost:3000/');
+});
+//inscription
+/*
 router.post('/', function(req,res, next){
-
+ console.log('1');
   var poolData = {
     UserPoolId : 'us-east-2_tjXnjbURk', // your user pool id here
     ClientId : '4tkakr53s85hrt7mpqve70bi0v' // your app client id here
@@ -29,23 +45,86 @@ router.post('/', function(req,res, next){
      Name : 'phone_number',
      Value : '+33668797666' // your phone number here with +country code and no delimiters in front
   };
+  var dataName = {
+    Name : 'name',
+    Value : 'ZijieAN' // your phone number here with +country code and no delimiters in front
+ };
+
   var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
   var attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(dataPhoneNumber);
-  
+  var attributeName = new AmazonCognitoIdentity.CognitoUserAttribute(dataName);
+  attributeList.push(attributeName);
   attributeList.push(attributeEmail);
   attributeList.push(attributePhoneNumber);
   
   var cognitoUser;
   userPool.signUp('ZijieAN', '515239aaBB', attributeList, null, function(err, result){
      if (err) {
-         alert(err);
+         console.log(attributeList);
+         console.log(err);
          return;
      }
      cognitoUser = result.user;
      console.log('user name is ' + cognitoUser.getUsername());
   });
+
   res.render('users', { title: 'Recette dans Oui Chef' });
 })
+*/
+//login
+/*
+router.post('/', function(req,res, next){
+var authenticationData = {
+  Username : req.body.username, // your username here
+  Password : req.body.password, // your password here
+};
+var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+var poolData = {
+  UserPoolId : 'us-east-2_tjXnjbURk', // your user pool id here
+  ClientId : '4tkakr53s85hrt7mpqve70bi0v' // your app client id here
+};
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+var userData = {
+  Username : req.body.username, // your username here
+  Pool : userPool
+};
+var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+cognitoUser.authenticateUser(authenticationDetails, {
+  onSuccess: function (result) {
+      var accessToken = result.getAccessToken().getJwtToken();
+      console.log(accessToken);
+
+      //POTENTIAL: Region needs to be set if not already set previously elsewhere.
+      AWS.config.region = 'us-east-2';
+
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+          IdentityPoolId : 'us-east-2_tjXnjbURk', // your identity pool id here
+          Logins : {
+              // Change the key below according to the specific region your user pool is in.
+              'cognito-idp.us-east-2.amazonaws.com/us-east-2_tjXnjbURk' : result.getIdToken().getJwtToken()
+          }
+      });
+
+      //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
+      AWS.config.credentials.refresh((error) => {
+          if (error) {
+               console.error(error);
+          } else {
+               // Instantiate aws sdk service objects now that the credentials have been updated.
+               // example: var s3 = new AWS.S3();
+               console.log('Successfully logged!');
+          }
+      });
+  },
+
+  onFailure: function(err) {
+      console.log(err.message || JSON.stringify(err));
+  },
+
+});
+})
+*/
 
 
 module.exports = router;
+
